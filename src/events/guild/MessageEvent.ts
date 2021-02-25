b83c8e1c25683f8ac8cf2b878e6983da2cb89dae
Doomer <queenofdoom@protonmail.com>
2021-02-25 22:57:36 +0100

    Gnight - I wanna slep!

--- src/events/guild/MessageEvent.ts
+++ src/events/guild/MessageEvent.ts
@@ -28,13 +28,34 @@ export const run: RunFunction = async(client, message: Message) => {
         message.channel.send("Meep.");
     }
 
-    if(message.author.id === "225814327242915841") {
-        var inpolite: string[] = ["fuck", "shut up", "hoe"];
+    var anti_swear_users: string[] = ["225814327242915841", "322069361852416005", "329113620921450497"];
+    if(anti_swear_users.indexOf(message.author.id) !== -1) {
+        var inpolite: string[] = ["fuck", "shut up", "hoe", "ass", "hell"];
         for(let i = 0; i < inpolite.length; i++) {
             if(message.content.toLowerCase().indexOf(inpolite[i]) !== -1) {
                 message.reply("<:baka:789210166184640552>");
             }
         }
+    }
+
+    var gnight: string[] = [
+        "goodnight everyone",
+        "night night everyone",
+        "see you all tomorrow",
+        "good night everyone",
+        "night night guys",
+        "good night guys",
+        "i slep now",
+        "i slep",
+        "night night",
+        "good night",
+        "i sleep",
+        "i sleep now",
+        "i'm heading to bed"
+    ];
+
+    if(gnight.indexOf(message.content.toLowerCase()) !== -1) {
+        message.channel.send("https://tenor.com/view/cute-kitten-alone-sleep-tucked-in-gif-14814077");
     }
 
     if(!message.content.toLowerCase().startsWith(prefix)) return;
