771890a461cecd8957f13e71e452d851763aa4d0
QueenOfDoom <71892989+QueenOfDoom@users.noreply.github.com>
2021-04-17 16:03:59 +0200

    fix: Correct Time Parsing

--- src/commands/general/UptimeCommand.ts
+++ src/commands/general/UptimeCommand.ts
@@ -3,9 +3,22 @@ import { start } from '../../client/ClientCache';
 import { RunFunction } from '../../interfaces/Command';
 
 export const run: RunFunction = async(client, message) => {
-    const time: Date = new Date(new Date().getTime()-start);
-    console.log("Got here!");
-    message.reply(`${client?.user?.tag} is online for ${time.toISOString().substr(11,8)}`);
+    const time: number = new Date().getTime() - start;
+    message.reply(`${client?.user?.tag} is online for ${formatTime(time)}`);
+}
+
+const formatTime = (ms: number) => {
+    const t_ms = ms % 1000;
+    ms -= t_ms; // Removes Millseconds
+    const t_s = ((ms)/1000) % 60;
+    ms -= (t_s * 1000); // Removes Seconds
+    const t_min = (ms/60000) % 60;
+    ms -= (t_min * 60000);
+    const t_hour = (ms/(60*60*1000)) % 24;
+    ms -= (t_hour * 60 * 60 * 1000);
+    const t_day = (ms/(24*60*60*1000));
+
+    return `${t_day}d, ${t_hour}h, ${t_min}m, ${t_s}s, ${t_ms}ms`;
 }
 
 export const name: string = 'uptime';
